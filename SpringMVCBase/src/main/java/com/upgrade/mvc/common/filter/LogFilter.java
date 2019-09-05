16da5f1fb9df2112eeaf5dc69363407350f1b2f7
leedy1100 <dy921119@gmail.com>
2019-09-05 21:47:04 +0900

    20190905 pm0947

--- SpringMVCBase/src/main/java/com/upgrade/mvc/common/filter/LogFilter.java
+++ SpringMVCBase/src/main/java/com/upgrade/mvc/common/filter/LogFilter.java
@@ -15,11 +15,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@WebFilter(filterName = "logFilter", urlPatterns = "/*")
+@WebFilter("/*")
 public class LogFilter implements Filter {
-	
-	private Logger logger = LoggerFactory.getLogger(Logger.class);
 
+	private Logger logger = LoggerFactory.getLogger(LogFilter.class);
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -30,13 +30,13 @@ public class LogFilter implements Filter {
 			throws IOException, ServletException {
 
 		HttpServletRequest req = (HttpServletRequest) request;
-
+		
 		String remoteAddr = StringUtils.defaultString(req.getRemoteAddr());
-
+		
 		String uri = StringUtils.defaultString(req.getRequestURI());
 		String url = StringUtils.defaultString(req.getRequestURL().toString());
 		String queryString = StringUtils.defaultString(req.getQueryString());
-
+		
 		String referer = StringUtils.defaultString(req.getHeader("referer"));
 		String agent = StringUtils.defaultString(req.getHeader("User-Agent"));
 		
@@ -54,10 +54,10 @@ public class LogFilter implements Filter {
 		.append("agent : "+agent);
 		
 		logger.info("[LOG FILTER]" + sb.toString());
-
+		
 		// 00 뺏어서 조작하고 다시 보내주는 역할(주고받기)
 		chain.doFilter(req, response);
-
+		
 	}
 
 	@Override
